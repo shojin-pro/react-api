3d239bfb7d1b8348200ef6720834bbe425377e3f
shojin-pro <shojin.pro@gmail.com>
2021-01-23 20:12:54 +0900

    first commit

---
+++ src/Pagination.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default function Pagenation({ gotoNextPage, gotoPrevPage}) {
+    return (
+        <div>
+            {gotoPrevPage && <button onClick={gotoPrevPage}>Previous</button>}
+            {gotoNextPage && <button onClick={gotoNextPage}>Next</button>}
+        </div>
+    )
+}
---
+++ src/PokemonList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default function PokemonList({ pokemon }){
+    return (
+        <div>
+            {pokemon.map(p => (
+                <div key={p}>{p}</div>
+            ))}
+        </div>
+    )
+}
